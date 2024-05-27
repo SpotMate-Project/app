e7ccb8c05fa6a31ed23c6b5b9984c14cb19ad142
박현수 <pm6567@naver.com>
2024-05-27 20:53:16 +0900

    feat : 회원가입 창 추가

---
+++ src/Agree/Agree.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { View, Text } from "react-native";
+
+const Agree: React.FC = () => {
+  return (
+    <View>
+      <Text>회원가입 창</Text>
+    </View>
+  );
+};
+
+export default Agree;
