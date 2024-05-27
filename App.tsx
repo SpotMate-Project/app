d7442895987b7ce053ea2c124faf3bead126de07
박현수 <pm6567@naver.com>
2024-05-27 18:53:48 +0900

    feat : Login page Component수정

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { View, Text, Image, TouchableOpacity, StyleSheet } from "react-native";
-
+import Login from "./src/Login/Login";
 type RootStackParamList = {
   Home: undefined;
   Login: undefined;
@@ -26,20 +26,12 @@ function HomeScreen({ navigation }: { navigation: any }) {
   );
 }
 
-function LoginScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Login Screen</Text>
-    </View>
-  );
-}
-
 function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Login" component={Login} />
       </Stack.Navigator>
     </NavigationContainer>
   );
