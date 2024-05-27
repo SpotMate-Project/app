c870406dce11550fff5127073b603f5d0db1f8c6
박현수 <pm6567@naver.com>
2024-05-27 21:07:22 +0900

    feat : App.tsx 의 라우터 추가

--- App.tsx
+++ App.tsx
@@ -3,9 +3,11 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { View, Text, Image, TouchableOpacity, StyleSheet } from "react-native";
 import Login from "./src/Login/Login";
+import Agree from "./src/Agree/Agree";
 type RootStackParamList = {
   Home: undefined;
   Login: undefined;
+  Argee: undefined;
 };
 
 const Stack = createStackNavigator<RootStackParamList>();
@@ -32,6 +34,7 @@ function App() {
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Agree" component={Agree} />
       </Stack.Navigator>
     </NavigationContainer>
   );
