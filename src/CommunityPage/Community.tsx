3d96a44dca1f2c74882bbba87117143bf8b0c658
박현수 <pm6567@naver.com>
2024-05-29 17:03:43 +0900

    feat : Map,Community 폴더 생성

---
+++ src/CommunityPage/Community.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { View, Text } from "react-native";
+
+const Community: React.FC = () => {
+  return (
+    <View>
+      <Text>커뮤니티페이지</Text>
+    </View>
+  );
+};
+
+export default Community;
---
+++ src/MapPage/Map.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { View, Text } from "react-native";
+
+const Map: React.FC = () => {
+  return (
+    <View>
+      <Text>Map페이지</Text>
+    </View>
+  );
+};
+
+export default Map;
