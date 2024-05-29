fa632ed9648c0c98a4886afce8e295998019b6bb
박현수 <pm6567@naver.com>
2024-05-30 02:45:31 +0900

    feat : 뒤로가기 추가

---
+++ src/MyPage/Setting/Setting.tsx
@@ -0,0 +1,143 @@
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useNavigation } from "@react-navigation/native";
+const SettingsPage: React.FC = () => {
+  const navigation = useNavigation();
+
+  const [nickname, setNickname] = useState<String>();
+  const [email, setEmail] = useState<String>();
+
+  const logout = async () => {
+    try {
+      await AsyncStorage.clear();
+      navigation.navigate("Login");
+    } catch (error) {
+      console.error("로그아웃 실패:", error);
+    }
+  };
+
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      try {
+        const storedEmail = await AsyncStorage.getItem("@user_email");
+        if (storedEmail) {
+          const response = await fetch(
+            "http://spotweb.hysu.kr:1030/user/info",
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                email: storedEmail,
+              }),
+            }
+          );
+          const data = await response.json();
+          if (data.success) {
+            setEmail(data.data[0].email);
+            setNickname(data.data[0].nickname);
+          }
+        }
+      } catch (error) {
+        console.error("유저정보가 없습니다:", error);
+      }
+    };
+
+    fetchUserInfo();
+  }, []);
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => navigation.goBack()}>
+        <Text>←</Text>
+      </TouchableOpacity>
+      <Text style={styles.headerText}>Settings</Text>
+      <View style={styles.profileContainer}>
+        <Image
+          source={require("../../../assets/profile.png")}
+          style={styles.profileImage}
+        />
+        <Text style={styles.userName}>{nickname}</Text>
+        <Text style={styles.userEmail}>{email}</Text>
+      </View>
+      <View style={styles.menuContainer}>
+        <TouchableOpacity style={styles.menuItem}>
+          <Text style={styles.menuText}>회원정보 수정</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.menuItem}>
+          <Text style={styles.menuText}>개발자 정보</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.menuItem} onPress={logout}>
+          <Text style={styles.menuLogout}>로그아웃</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.bottomLine} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "#fff",
+    paddingTop: 80,
+  },
+  headerText: {
+    fontFamily: "Jua",
+    fontSize: 30,
+    textAlign: "center",
+    color: "#00BCD4",
+    marginVertical: 5,
+  },
+  profileContainer: {
+    alignItems: "center",
+    marginTop: 20,
+  },
+  profileImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    backgroundColor: "#ddd",
+  },
+  userName: {
+    fontFamily: "Jua",
+    fontSize: 25,
+    color: "#00BCD4",
+    marginTop: 10,
+  },
+  userEmail: {
+    fontFamily: "Jua",
+    fontSize: 18,
+    color: "#555",
+    marginTop: 5,
+  },
+  menuContainer: {
+    flex: 1,
+    marginTop: 50,
+  },
+  menuItem: {
+    padding: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ddd",
+  },
+  menuText: {
+    fontFamily: "Jua",
+    fontSize: 20,
+    color: "#00BCD4",
+  },
+  menuLogout: {
+    fontFamily: "Jua",
+    fontSize: 20,
+    color: "red",
+  },
+  bottomLine: {
+    width: "100%",
+    height: 4,
+    backgroundColor: "#00BCD4",
+    marginVertical: 20,
+  },
+});
+
+export default SettingsPage;
