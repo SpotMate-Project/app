3f58f1a9978b895f968c35ac718e1f72521e9017
박현수 <pm6567@naver.com>
2024-05-29 16:05:25 +0900

    refatcor : return 문 <> 감싸고 mainpage -> MainPage uppercase 로 수정

--- src/MainPage/MainPage.tsx
+++ src/MainPage/MainPage.tsx
@@ -1,58 +1,92 @@
-import React from 'react';
-import { StyleSheet, Text, View, ScrollView, TouchableOpacity, Image } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  TouchableOpacity,
+  Image,
+} from "react-native";
 
-const mainpage: React.FC = () => {
+const MainPage: React.FC = () => {
   return (
-    <ScrollView style={styles.container}>
-      <Text style={styles.headerText}>Spotmate</Text>
+    <>
+      <ScrollView style={styles.container}>
+        <Text style={styles.headerText}>Spotmate</Text>
 
-      <View style={styles.banner}>
-        <Image source={require('../../assets/gear.png')} style={styles.bannerImage} />
-        <Text style={styles.bannerText}>우리 동네 주변에 뭐가 있지?</Text>
-      </View>
-
-      <View style={styles.section}>
-        <View style={styles.categoryContainer}>
-          <TouchableOpacity style={styles.categoryItem}>
-            <Image source={require('../../assets/gear.png')} style={styles.categoryIcon} />
-            <Text style={styles.categoryText}>관심 카테고리</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.categoryItem}>
-            <Image source={require('../../assets/gear.png')} style={styles.categoryIcon} />
-            <Text style={styles.categoryText}>리뷰</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.categoryItem}>
-            <Image source={require('../../assets/gear.png')} style={styles.categoryIcon} />
-            <Text style={styles.categoryText}>핫 플레이스</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.categoryItem}>
-            <Image source={require('../../assets/gear.png')} style={styles.categoryIcon} />
-            <Text style={styles.categoryText}>추천 스팟</Text>
-          </TouchableOpacity>
+        <View style={styles.banner}>
+          <Image
+            source={require("../../assets/gear.png")}
+            style={styles.bannerImage}
+          />
+          <Text style={styles.bannerText}>우리 동네 주변에 뭐가 있지?</Text>
         </View>
+
+        <View style={styles.section}>
+          <View style={styles.categoryContainer}>
+            <TouchableOpacity style={styles.categoryItem}>
+              <Image
+                source={require("../../assets/gear.png")}
+                style={styles.categoryIcon}
+              />
+              <Text style={styles.categoryText}>관심 카테고리</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.categoryItem}>
+              <Image
+                source={require("../../assets/gear.png")}
+                style={styles.categoryIcon}
+              />
+              <Text style={styles.categoryText}>리뷰</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.categoryItem}>
+              <Image
+                source={require("../../assets/gear.png")}
+                style={styles.categoryIcon}
+              />
+              <Text style={styles.categoryText}>핫 플레이스</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.categoryItem}>
+              <Image
+                source={require("../../assets/gear.png")}
+                style={styles.categoryIcon}
+              />
+              <Text style={styles.categoryText}>추천 스팟</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ScrollView>
+      <View style={styles.bottomLine} />
+      <View style={styles.bottomMenuContainer}>
+        <TouchableOpacity style={styles.bottomMenuItem}>
+          <Image
+            source={require("../../assets/zip.png")}
+            style={styles.bottomMenuIcon}
+          />
+          <Text style={styles.bottomMenuText}>홈</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.bottomMenuItem}>
+          <Image
+            source={require("../../assets/map2.png")}
+            style={styles.bottomMenuIcon}
+          />
+          <Text style={styles.bottomMenuText}>MAP</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.bottomMenuItem}>
+          <Image
+            source={require("../../assets/munity.png")}
+            style={styles.bottomMenuIcon}
+          />
+          <Text style={styles.bottomMenuText}>커뮤니티</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.bottomMenuItem}>
+          <Image
+            source={require("../../assets/human.png")}
+            style={styles.bottomMenuIcon}
+          />
+          <Text style={styles.bottomMenuText}>마이페이지</Text>
+        </TouchableOpacity>
       </View>
-    </ScrollView>
-</View>
-<View style={styles.bottomLine} />
-<View style={styles.bottomMenuContainer}>
-  <TouchableOpacity style={styles.bottomMenuItem}>
-    <Image source={require('../../assets/zip.png')} style={styles.bottomMenuIcon} />
-    <Text style={styles.bottomMenuText}>홈</Text>
-  </TouchableOpacity>
-  <TouchableOpacity style={styles.bottomMenuItem}>
-    <Image source={require('../../assets/map2.png')} style={styles.bottomMenuIcon} />
-    <Text style={styles.bottomMenuText}>MAP</Text>
-  </TouchableOpacity>
-  <TouchableOpacity style={styles.bottomMenuItem}>
-    <Image source={require('../../assets/munity.png')} style={styles.bottomMenuIcon} />
-    <Text style={styles.bottomMenuText}>커뮤니티</Text>
-  </TouchableOpacity>
-  <TouchableOpacity style={styles.bottomMenuItem}>
-    <Image source={require('../../assets/human.png')} style={styles.bottomMenuIcon} />
-    <Text style={styles.bottomMenuText}>마이페이지</Text>
-  </TouchableOpacity>
-  </View>
-</View>
+    </>
   );
 };
 
@@ -60,42 +94,42 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
   headerText: {
-    fontFamily: 'Jua',
+    fontFamily: "Jua",
     fontSize: 30,
-    textAlign: 'center',
-    color: '#00BCD4',
+    textAlign: "center",
+    color: "#00BCD4",
     marginVertical: 5,
   },
   banner: {
-    alignItems: 'center',
+    alignItems: "center",
     marginVertical: 10,
   },
   bannerImage: {
-    width: '100%',
+    width: "100%",
     height: 150,
     borderRadius: 10,
   },
   bannerText: {
-    fontFamily: 'Jua',
+    fontFamily: "Jua",
     fontSize: 18,
-    color: '#000',
-    textAlign: 'center',
+    color: "#000",
+    textAlign: "center",
     marginTop: 10,
   },
   section: {
     marginVertical: 10,
   },
   categoryContainer: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
   },
   categoryItem: {
-    width: '45%',
-    alignItems: 'center',
+    width: "45%",
+    alignItems: "center",
     marginVertical: 10,
   },
   categoryIcon: {
@@ -103,11 +137,33 @@ const styles = StyleSheet.create({
     height: 60,
   },
   categoryText: {
-    fontFamily: 'Jua',
+    fontFamily: "Jua",
     fontSize: 18,
-    color: '#00BCD4',
+    color: "#00BCD4",
+    marginTop: 5,
+  },
+  bottomLine: {
+    height: 1,
+    backgroundColor: "#ddd",
+    marginVertical: 10,
+  },
+  bottomMenuContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  bottomMenuItem: {
+    alignItems: "center",
+  },
+  bottomMenuIcon: {
+    width: 30,
+    height: 30,
+  },
+  bottomMenuText: {
+    fontFamily: "Jua",
+    fontSize: 14,
+    color: "#00BCD4",
     marginTop: 5,
   },
 });
 
-export default mainpage;
+export default MainPage;
