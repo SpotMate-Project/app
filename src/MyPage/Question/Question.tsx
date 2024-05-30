5441780a13cf76b2ab02eaa5cc20223a5efb566c
presio10 <rkdgh7916@gmail.com>
2024-05-30 22:35:04 +0900

    feat: 문의사항

--- src/MyPage/Question/Question.tsx
+++ src/MyPage/Question/Question.tsx
@@ -1,15 +1,41 @@
 import React from "react";
-import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
 const Question: React.FC = () => {
   const navigation = useNavigation();
+
   return (
     <View style={styles.container}>
       <TouchableOpacity onPress={() => navigation.goBack()}>
-        <Text>←</Text>
+        <Text style={styles.backButton}>←</Text>
       </TouchableOpacity>
-      <Text>문의사항 제작해야함</Text>
+      <Text style={styles.headerText}>문의사항</Text>
+      <ScrollView style={styles.contentContainer}>
+        <TouchableOpacity style={styles.item}>
+          <Text style={styles.itemText}>자주 묻는 질문</Text>
+        </TouchableOpacity>
+        <View style={styles.line} />
+        <TouchableOpacity style={styles.item}>
+          <Text style={styles.itemText}>1:1 문의</Text>
+        </TouchableOpacity>
+        <View style={styles.line} />
+        <TouchableOpacity style={styles.item}>
+          <Text style={styles.itemText}>이메일 문의</Text>
+        </TouchableOpacity>
+        <View style={styles.line} />
+        <TouchableOpacity style={styles.item}>
+          <Text style={styles.itemText}>공지사항</Text>
+        </TouchableOpacity>
+        <View style={styles.line} />
+      </ScrollView>
+      <View style={styles.bottomLine} />
     </View>
   );
 };
@@ -19,7 +45,43 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 16,
     backgroundColor: "#fff",
-    paddingTop: 100,
+    paddingTop: 50,
+  },
+  backButton: {
+    fontSize: 24,
+    color: "#00BCD4",
+  },
+  headerText: {
+    fontFamily: "Jua",
+    fontSize: 30,
+    textAlign: "center",
+    color: "#00BCD4",
+    marginVertical: 5,
+  },
+  contentContainer: {
+    flex: 1,
+    marginTop: 120,
+  },
+  item: {
+    paddingVertical: 10
+    
+  },
+  itemText: {
+    fontFamily: "Jua",
+    fontSize: 22,
+    color: "#00BCD4",
+    textAlign: "center",
+  },
+  line: {
+    height: 1,
+    backgroundColor: "#00BCD4",
+    marginVertical: 10,
+  },
+  bottomLine: {
+    width: "100%",
+    height: 4,
+    backgroundColor: "#00BCD4",
+    marginVertical: 20,
   },
 });
 
